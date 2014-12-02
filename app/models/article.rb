1c95bb74dfd951fbaef5db0eab79c8c4df2dc6d4
Erich Rahm <erahm84@gmail.com>
2014-12-01 21:22:27 -0700

    adding categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,15 +2,18 @@
 #
 # Table name: articles
 #
-#  id         :integer          not null, primary key
-#  title      :string(255)
-#  body       :text
-#  published  :boolean
-#  created_at :datetime
-#  updated_at :datetime
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  body        :text
+#  published   :boolean
+#  created_at  :datetime
+#  updated_at  :datetime
+#  category_id :integer
 #
 
 class Article < ActiveRecord::Base
-  validates_presence_of :title, :body
+  belongs_to :category
+
+  validates_presence_of :title, :body, :category_id
   validates_uniqueness_of :title
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BlogRails::Application.routes.draw do
+  resources :categories
+
   devise_for :users
   mount Ckeditor::Engine => '/ckeditor'
   resources :articles
